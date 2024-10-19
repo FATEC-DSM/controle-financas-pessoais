65418198252ac2270d3793ffda718393582748d1
Maxwell-Santos <max.coding011@gmail.com>
2024-10-18 21:23:56 -0300

    Início da tela de cadastrar usuário

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RecoverPasswordComponent } from './recover-password/recover-password.component';
+import { RegisterUserComponent } from './register-user/register-user.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'recover', component: RecoverPasswordComponent },
+  { path: 'register-user', component: RegisterUserComponent },
 ];
